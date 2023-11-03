00224c8bab56ee35c4da31b1d6df7008ff8e8f7d
JanyaRK <jr2068@hw.ac.uk>
2023-11-03 10:33:57 +0400

    aa

--- week 4/main.js
+++ week 4/main.js
@@ -2,6 +2,6 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET', 'https://janyark.github.io/F28WP-Lab01/week%204/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
-console.log(ourData[0]);
+console.log(ourData);
 };
 ourRequest.send();
